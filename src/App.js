840832756ca064a75b687909f854dc72d0be5c6a
Tallal <=>
2023-01-26 11:43:37 +0500

    setstate to change the name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Tallal",
+      company: "Tecaudex"
+    }
+
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi {this.state.name}, I work at {this.state.company}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <button onClick={() => {
+        this.setState({ name: "Ali" });
+       }}>Change name</button>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
