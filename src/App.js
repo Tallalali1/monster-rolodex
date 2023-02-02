8c3705df71208da9ad08a0c8c632e7f8a781ac95
Tallal <=>
2023-02-02 14:20:13 +0500

    optimization

--- src/App.js
+++ src/App.js
@@ -22,21 +22,24 @@ class App extends Component {
     )
     )
   }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    })
+  }
 
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField);
+    const {monsters, searchField } = this.state
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
     });
   return (
     <div className="App">
     <input className='search-box' type='search' placeholder='search monsters' 
-    onChange={(event) => {
-      const searchField = event.target.value.toLowerCase();
-
-      this.setState(() => {
-        return { searchField };
-      })
-    }} />
+    onChange={onSearchChange} />
     {
       filteredMonsters.map((monster) => {
         return <h1>{monster.name}</h1>
