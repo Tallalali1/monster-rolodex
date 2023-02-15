46f1b3d363d7728f3820222ab2181e23c0fed15e
Tallal Irfan <89486859+Tallalali1@users.noreply.github.com>
2023-02-15 15:19:12 +0500

    Refactor/card list component (#6)
    * added images in user cards and styling
    
    * made a card component
    
    * made the style for my monster app
    
    ---------
    
    Co-authored-by: Tallal <=>

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Card from "../card/card.component";
 import './card-list.styles.css'
 
 class CardList extends Component {
@@ -7,16 +8,11 @@ class CardList extends Component {
       const { monsters } = this.props;
       return (
         <div className='card-list'>
-          {monsters.map((monster) => {
-            const { name, email, id} = monster
-            return (
-            <div className="card-container">
-            <img alt={ `monster ${ name }` } src={ `https://robohash.org/${id}?set=set2&size=180x180` } />
-            <h2>{name}</h2>
-            <p>{email}</p>
-            </div>
-          )})}
-        </div>
+        {monsters.map((monster) => {
+          return (
+            <Card monster={monster}/>
+        )})}
+      </div>
       );
     }
   }
