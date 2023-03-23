350d12f67a59f3a906ec3381f7b60b15ba011012
Tallal <=>
2023-03-23 15:25:29 +0500

    now did search box component

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,18 +1,12 @@
-import { Component } from "react";
-
-
 import './search-box.styles.css';
 
-class SearchBox extends Component {
-    render() {
-        return(
-            <input className={`search-box ${this.props.className}`}
+const SearchBox = ({className, placeholder, onChangeHandler }) => (
 
-             type='search' 
-             placeholder={this.props.placeholder}
-            onChange={this.props.onChangeHandler} />
-        )
-    }
-}
+    <input className={`search-box ${className}`}
+    
+        type='search' 
+        placeholder={placeholder}
+        onChange={onChangeHandler} />
+    )
 
 export default SearchBox;
