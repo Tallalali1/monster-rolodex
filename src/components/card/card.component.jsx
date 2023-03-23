fd109b6e3740f3623b72e4f3402dd29061883e1c
Tallal <=>
2023-03-23 15:17:40 +0500

    chaning all components into functional components done card component now

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,18 +1,14 @@
-import { Component } from "react";
-
 import './card.styles.css';
 
-class Card extends Component {
-render() {
-    const {id, name, email, } = this.props.monster
+const Card = ({monster}) => {
+    const {id, name, email } = monster
     return(
-        <div className="card-container">
+    <div className="card-container">
         <img alt={ `monster ${ name }` } src={ `https://robohash.org/${id}?set=set2&size=180x180` } />
         <h2>{name}</h2>
         <p>{email}</p>
-        </div>
+    </div>
     )
     }
-}
 
 export default Card;
